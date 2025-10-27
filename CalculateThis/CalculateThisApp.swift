8a496f330ed8cc4cef6b5f767ce825fb9feda599
psiegel18 <preston.siegel@gmail.com>
2025-10-27 02:35:20 -0500

    Initial Commit

---
+++ CalculateThis/CalculateThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateThisApp.swift
+//  CalculateThis
+//
+//  Created by Preston Siegel on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculateThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculateThisTests/CalculateThisTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculateThisTests.swift
+//  CalculateThisTests
+//
+//  Created by Preston Siegel on 10/27/25.
+//
+
+import Testing
+
+struct CalculateThisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
